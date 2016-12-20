3bfd8746061bbe30ce5c07906e7e85d92f0c69ed
Angel Newton <xanewton@gmail.com>
2016-12-19 20:31:15 -0600

    Update UnitTest

---
+++ IdealTests/GeneralUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IdealTests
+{
+    /// <summary>
+    /// General Tests
+    /// </summary>
+    [TestClass]
+    public class GeneralUnitTest
+    {
+        private String SAMPLE_DATABASE = "";
+
+
+        [TestInitialize]
+        public void Initialization()
+        {
+            // Find the sample database path
+            string proyectPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            SAMPLE_DATABASE = proyectPath.Replace("IdealTests", "Ideal\\SampleDatabase.mdf");
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            SAMPLE_DATABASE = "";
+        }
+
+
+        /// <summary>
+        /// Checks that the sample database is there.
+        /// </summary>
+        [TestMethod]
+        public void TestSampleDatabaseExists()
+        {
+            Assert.AreEqual(true, File.Exists(SAMPLE_DATABASE));
+        }
+    }
+}
